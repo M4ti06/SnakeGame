ceac1420fffb6a4640cbdba0da76f15347672790
M4ti06 <104864383+M4ti06@users.noreply.github.com>
2024-03-12 21:11:25 +0100

    highscore

--- scoreboard.py
+++ scoreboard.py
@@ -13,14 +13,23 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 260)
         self.score = 0
-        self.write(f"Score:{self.score}", align=ALIGNMENT, font=FONT)
+        with open("data.txt") as data:
+            self.highscore = int(data.read())
+        self.write(f"Score:{self.score} Highscore: {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def update_scoreboard(self):
-        self.write(f"Score:{self.score}", align="center", font=("Arial", 24, "normal"))
+        self.clear()
+        self.write(f"Score:{self.score} Highscore: {self.highscore}", align="center", font=("Arial", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.clear()
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.highscore:
+            with open("data.txt", "w") as data:
+                data.write(f"{self.highscore}")
+        self.score = 0
         self.update_scoreboard()
 
     def game_over(self):
--- snake.py
+++ snake.py
@@ -36,6 +36,14 @@ class Snake:
         # Add  new segment to the snake.
         self.add_segment(self.segments[-1].position())
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(90)
